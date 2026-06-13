f706d1528e32320354c00f38a9cb481d678dbb91
ibfaye <iboufaye2000@hotmail.com>
2026-06-13 09:48:10 -0400

    fix: {# #} Jinja comments prevent dbt parsing, remove deprecated snapshot macro

--- dbt/snapshots/customers_scd.sql
+++ dbt/snapshots/customers_scd.sql
@@ -1,12 +1,15 @@
--- Placeholder: SCD Type 2 snapshot template
--- Uncomment and customize for your slowly-changing dimensions:
---
--- {% snapshot customers_scd %}
---   {{ config(
---       target_schema='snapshots',
---       unique_key='customer_id',
---       strategy='timestamp',
---       updated_at='_loaded_at'
---   ) }}
---   SELECT * FROM {{ ref('customers_cleaned') }}
--- {% endsnapshot %}
+{# SCD Type 2 snapshot for customer profiles.
+   Tracks changes to customer data over time.
+   Uncomment to activate. #}
+{# 
+{% snapshot customers_scd %}
+  {{ config(
+      target_schema='snapshots',
+      unique_key='customer_id',
+      strategy='timestamp',
+      updated_at='_loaded_at',
+      invalidate_hard_deletes=true
+  ) }}
+  SELECT * FROM {{ ref('customers_cleaned') }}
+{% endsnapshot %}
+#}
